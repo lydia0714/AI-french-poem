5a42eb6c36f16ee99c5a3337cfb89f3e29662e32
lydia0714 <lvhunga@yahoo.com>
2024-09-21 21:16:43 +0200

    added the api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function displayPoem(response) {
+    new Typewriter("#poem", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+
+
 function generatePoem(event){
     event.preventDefault ();
 
-    let poemElement = document.querySelector ("#poem");
-    poemElement.innerHTML = "french poem";
+    let instructionsInput = document.querySelector("#user-instructions");
+
+    let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let context =
+    "You are an expert writer and love to write poems. Your goal is to write a 4 line poem and display it in basic HTML structure, but without the html and body tags and separate each line. Be sure of generate a different poem every time. Do not add a title";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiURL).then(displayPoem);
 }
 
 
