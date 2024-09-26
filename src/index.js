aaeb29619b828e3a2d85b3800596fd293719e67d
lydia0714 <lvhunga@yahoo.com>
2024-09-26 03:48:46 +0200

    Updated html

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function generatePoem(event){
 
     let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
     axios.get(apiURL).then(displayPoem);
 }
