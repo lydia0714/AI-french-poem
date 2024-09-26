3b5dfa039585e73c2cd74978b5f29c6fbec6e3a4
lydia0714 <lvhunga@yahoo.com>
2024-09-26 03:05:18 +0200

    added java

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generatePoem(event){
     "You are an expert writer and love to write poems. Your goal is to write a 4 line poem and display it in basic HTML structure, but without the html and body tags and separate each line. Be sure of generate a different poem every time. Do not add a title";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
     axios.get(apiURL).then(displayPoem);
 }
 
